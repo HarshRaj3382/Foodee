3f0d4a4887524751fc31239ffc0e99b5d9563c80
Harsh Raj <harshjpur41@gmail.com>
2025-05-09 17:39:04 +0530

    built path try

--- src/utils/appStore.jsx
+++ src/utils/appStore.jsx
@@ -13,8 +13,8 @@
 // export default appStore;
 
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cartSlice.jsx";
-import authReducer from "./authslice.jsx";
+import cartReducer from "./cartSlice";
+import authReducer from "./authslice";
 
 const appStore = configureStore({
   reducer: {
